49bc60d3d7cb1714963429db40daa6684a889edd
hseirafi <hseirafi@gmail.com>
2019-06-18 14:19:18 -0700

    update

--- heavyProcessing.cpp
+++ heavyProcessing.cpp
@@ -1,5 +1,7 @@
 #include <nan.h>
+#include <string>
 
+using namespace v8;
 void Sum(const v8::FunctionCallbackInfo<v8::Value>& args){
 
 v8::Isolate* isolate= args.GetIsolate();
@@ -10,13 +12,18 @@ for(i=0; i<1000000; i++){
 a += b;
 
 }
-
-auto total = v8::Number::New(isolate, a); 
-args.GetReturnValue().Set(total); 
-
+std::string sText = "world you can suck it";
+// auto total = v8::Number::New(isolate, a); 
+// args.GetReturnValue().Set(total); 
+//auto hTextJS = v8::String::NewFromUtf8(isolate, sText.c_str(), v8::NewStringType::kNormal );
+Local<String> retval = String::NewFromUtf8(isolate, sText.c_str());
+  args.GetReturnValue().Set(retval);
+//  args.GetReturnValue().Set(v8::String::NewFromUtf8(
+//       isolate, "helll world ", v8::NewStringType::kNormal).ToLocalChecked());
 }
 
-void Initialize(v8::Local<v8::Object> exports){
+
+void Initialize(Local<Object> exports){
     NODE_SET_METHOD(exports, "sum", Sum);
 }
 
